7faad833b6298f15c3d6298a3fbc4d8ab2b74dd5
nkizito92 <nkizito57@gmail.com>
2022-01-27 20:18:19 -0600

    assets has images and cars data for props

--- assets/styles.js
+++ assets/styles.js
@@ -1,37 +1,10 @@
-import React from "react"
 import { StyleSheet } from "react-native"
 const styling = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
-    },
-    carContainer: {
-        width: '100%',
-        height: '100%',
-    },
-    titles: {
-        marginTop: '30%',
-        width: '100%',
-        alignItems: 'center'
-    },
-    title: {
-        fontSize: 40,
-        fontWeight: '600'
-    },
-    subtitle: {
-        fontSize: 20,
-        alignItems: 'center',
-        color: '#5c5e62'
-    },
-
-    image: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover',
-        position: 'absolute',
     }
-
 
 })
 
