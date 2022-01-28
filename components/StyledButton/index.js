59bd8b0ebd9b0747b8b9bef4c26d51af9ac3da0f
nkizito92 <nkizito57@gmail.com>
2022-01-27 18:37:48 -0600

    set conditions for the background and text along with new props

---
+++ components/StyledButton/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, Pressable } from 'react-native'
+import styles from "./styles";
+//***********************deconstructed properties below*************/
+const StyledButton = ({ type, content, onPress }) => {
+    // setting the color best on the value in the pressable button
+    const backgroundColor = type === 'primary' ? '#171A20CC' : 'blue'
+    const textColor = type === 'primary' ? 'white' : '#171A20CC'
+
+    return (
+        <View style={styles.container}>
+            <Pressable style={[styles.button,
+            { backgroundColor: backgroundColor }]} onPress={() => onPress()}>
+                <Text style={[styles.text, { color: textColor }]}>{content}</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default StyledButton;
