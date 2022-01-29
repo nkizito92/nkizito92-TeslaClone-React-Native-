ff56ea99ac5f5d84d7ed69f70b62a9a157f10e0c
nkizito92 <nkizito57@gmail.com>
2022-01-28 18:54:41 -0600

    created a header component

---
+++ components/Header/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native'
+const styles = StyleSheet.create({
+
+    container: {
+        position: 'absolute',
+        top: 50,
+        zIndex: 120,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+    },
+
+    text: {
+        color: 'white'
+    },
+
+    logo: {
+        width: 100,
+        height: 20,
+        resizeMode: 'contain',
+
+    },
+    menu: {
+        width: 25,
+        height: 25,
+        resizeMode: 'contain',
+    }
+
+})
+export default styles
