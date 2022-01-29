2879e42d7f3bf4c120072b94cd5f83e8f483a4f0
nkizito92 <nkizito57@gmail.com>
2022-01-28 18:55:11 -0600

    import the Header component

--- App.js
+++ App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import {
   Text,
   View,
-  ImageBackground
+  ImageBackground,
+  StatusBar
 } from 'react-native';
 import styling from './assets/styles';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 
 const App = () => {
   return (
     <View style={styling.container}>
-      <View style={styling.carContainer}>
-        <ImageBackground
-          style={styling.image}
-          source={require('./assets/images/assets/images/Model3.jpeg')} />
-        <View style={styling.titles}>
-          <Text style={styling.title}>Model S</Text>
-          <Text style={styling.subtitle}>Starting at $63,981</Text>
-        </View>
-      </View>
+      <CarsList />
+      <StatusBar />
+      <Header />
     </View>
   )
 };
