eadf20501ce3bcab84c11b631d5f7a93de126ad6
nkizito92 <nkizito57@gmail.com>
2022-01-28 18:57:40 -0600

    updated CarItem props for the Flatlist component in CarsList

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,16 +1,33 @@
 import React from 'react'
-import { View, Text, ImageBackground } from 'react-native'
+import { View, Text, ImageBackground, StatusBar } from 'react-native'
 import styles from './styles'
-
-const CarItem = ({ }) => {
+import StyledButton from '../StyledButton'
+const CarItem = (props) => {
+    const { name, tagline, image, taglineCTA } = props.car
     return (
         <View style={styles.carContainer}>
             <ImageBackground
                 style={styles.image}
-                source={require('../../assets/images/assets/images/Model3.jpeg')} />
+                source={image} />
             <View style={styles.titles}>
-                <Text style={styles.title}>Model S</Text>
-                <Text style={styles.subtitle}>Starting at $63,981</Text>
+
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.subtitle}>{tagline}{" "}
+                    <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+                </Text>
+
+                <StatusBar />
+            </View>
+            <View style={styles.buttonsContainer}>
+                <StyledButton type='primary'
+                    content={"Custom Order"}
+                    onPress={() => {
+                        console.warn('Custom Order was pressed!!')
+                    }} />
+                <StyledButton type='secondary' content={"Existing Inventory"}
+                    onPress={() => {
+                        console.warn('Existing Inventory was pressed!!!!')
+                    }} />
             </View>
         </View>
     )
--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,8 +1,17 @@
-import { StyleSheet } from "react-native"
+import { Dimensions, StyleSheet } from "react-native"
 const styles = StyleSheet.create({
     carContainer: {
         width: '100%',
-        height: '100%',
+        /* use Dimension for single page apps to scroll to next page 
+          like word documents*/
+        height: Dimensions.get('window').height
+    },
+    buttonsContainer: {
+        position: 'absolute',
+        alignItems: 'center',
+        top: '80%',
+        width: '100%',
+        zIndex: 20
     },
     titles: {
         marginTop: '30%',
@@ -18,12 +27,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         color: '#5c5e62'
     },
+    subtitleCTA: {
+        textDecorationLine: 'underline'
+    },
 
     image: {
         width: '100%',
         height: '100%',
         resizeMode: 'cover',
         position: 'absolute',
+        zIndex: 0
     }
 
 
